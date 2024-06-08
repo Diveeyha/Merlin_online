997f13fcf5ef8b478dd0783b628b68d9237b5ef6
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2024-06-08 16:54:25 -0400

    Update Merlin_online.py

--- Merlin_online.py
+++ Merlin_online.py
@@ -106,8 +106,9 @@ def load_model(tf_model):
     # Load TFLite model and allocate tensors.
     # url = 'https://raw.githubusercontent.com/Diveeyha/Merlin_online/main/model/' + tf_model + '.tflite'
     url = '/model/' + tf_model + '.tflite'
-    model_path = open(url, "r")
-    myinterpreter = tf.lite.Interpreter(model_path=model_path)
+    response = requests.get(url)
+    model_open = open(response, "r")
+    myinterpreter = tf.lite.Interpreter(model_path=model_open)
     myinterpreter.allocate_tensors()
 
     # Get input and output tensors.
